94e23b3ef75d030fa02cd2a7885712862cd23a3b
Robinson Segura Aponte <56366357+robinsons1@users.noreply.github.com>
2025-05-23 14:45:42 -0500

    Actualización automática

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Montar carpeta estática y templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-# Ya no necesitas inicializar firebase-admin ni subir datos periódicos en backend
